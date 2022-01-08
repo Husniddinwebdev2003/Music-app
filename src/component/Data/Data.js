2e04faa59b4943b43636bbf541edad84161c602d
Husniddinwebdev2003 <87707617+Husniddinwebdev2003@users.noreply.github.com>
2022-01-08 18:45:44 +0300

    added data

---
+++ src/component/Data/Data.js
@@ -0,0 +1,48 @@
+export default function Data() {
+    return [
+            {
+                musicName: "Beaver Creek",
+                artist: "Aso, Middle School, Aviino",
+                urlMusic: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+                urlImg: "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+                id: (Math.random() + "sds" + Math.random())
+            },
+            
+            {
+                musicName: "Daylight",
+                artist: "Aiguille",
+                urlMusic: "https://mp3.chillhop.com/serve.php/?mp3=9272",
+                urlImg: "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+                id: (Math.random() + "sds" + Math.random())
+            },
+            {
+                musicName: "Keep Going",
+                artist: "Swørn",
+                urlMusic: "https://mp3.chillhop.com/serve.php/?mp3=9222",
+                urlImg: "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+                id: (Math.random() + "sds" + Math.random())
+            },
+            {
+                musicName: "Nightfall",
+                artist: "Aiguille",
+                urlMusic: "https://mp3.chillhop.com/serve.php/?mp3=9148",
+                urlImg: "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+                id: (Math.random() + "sds" + Math.random())
+            },
+            {
+                musicName: "Reflection",
+                artist: "Swørn",
+                urlMusic: "https://mp3.chillhop.com/serve.php/?mp3=9228",
+                urlImg: "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+                id: (Math.random() + "sds" + Math.random())
+            },
+            {
+                musicName: "Under the City Stars",
+                artist: "Aso, Middle School, Aviino",
+                urlMusic: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+                urlImg: "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+                id: (Math.random() + "sds" + Math.random())
+            }
+        ];
+}
+
